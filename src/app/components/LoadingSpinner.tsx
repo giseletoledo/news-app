b40a1bf003beb4ba8fababc842293ef32e9c6d8f
giseletoledo <gisesonia@gmail.com>
2024-06-10 16:27:14 -0300

    Componente de carregar noticias

---
+++ src/app/components/LoadingSpinner.tsx
@@ -0,0 +1,14 @@
+
+import './LoadingSpinner.css';
+
+const LoadingSpinner = () => {
+    return (
+      <div className="spinner">
+        <div className="dot1"></div>
+        <div className="dot2"></div>
+        <div className="dot3"></div>
+      </div>
+    );
+  };
+  
+  export default LoadingSpinner;
